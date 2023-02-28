81995277c950035ea4882625d14cfedb4d95636c
Vittu Singh <vittu.singh@sparkeighteen.com>
2023-02-28 17:28:51 +0530

    refactor

--- jsRendering.js
+++ jsRendering.js
@@ -1,6 +1,11 @@
 // Rendering into markup Using Javascript
 
-const heading = document.createElement("h1");
-heading.innerHTML = "Namaste Everyone from JavaScript";
+const jsHeading = document.createElement("h1");
+jsHeading.innerHTML = "Namaste Everyone from JavaScript";
 const root1 = document.getElementById("root");
-root1.appendChild(heading);
+root1.appendChild(jsHeading); 
+
+
+// react elements are immutable , cannot change react element using javascript after it has been rendered using react
+// If the element is made first using javascript, then react is used to change its contents,
+//  then react overwrites the root's contents
