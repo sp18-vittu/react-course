52ad7b07478aded3c211bdd51b46ddf8b9982a6b
Vittu Singh <vittu.singh@sparkeighteen.com>
2023-03-13 17:25:28 +0530

    refactor

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -52,7 +52,7 @@ function Body() {
 
   const SearchedRestaurantNotFound = ({ searchedText }) => {
     return (
-      <h3>
+      <h3 style={{ height: "63vh" }}>
         {`Sorry, we couldn't find any restaurant named " ` +
           (searchedText.length > 13
             ? `${searchedText.slice(0, 8)}....${searchedText.slice(
